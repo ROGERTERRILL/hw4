2e4da0d927af9088b4c04b9e6b07d9b33bd852c0
Roger Dodger <lpaddikt@gmail.com>
2019-11-13 22:08:21 -0800

    WIP faker not working

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req, res) {
+  res.render("index.html");
+});
+
+app.get("/iloveyou", function(req, res) {
+  res.render("iloveyou.html");
+});
+
+app.get("/shamoon", function(req, res) {
+  res.render("shamoon.html");
+});
+
+app.get("/stuxnet", function(req, res) {
+  res.render("stuxnet.html");
+});
+
+//server listener
+app.listen(
+  process.env.PORT || "8081",
+  process.env.IP || "localhost",
+  function() {
+    console.log("Express server is running...");
+  }
+);
